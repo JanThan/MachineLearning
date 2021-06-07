dda5b6a741e56b5693f70fe782134a7ae2accc2c
JanThan <49203245+JanThan@users.noreply.github.com>
2021-06-07 23:16:53 +0100

    Add files via upload

---
+++ 3rd Year Group Robotics Project/VisualErrorCode.py
@@ -0,0 +1,193 @@
+# Error Codes:
+# normal    : red, blue, green
+# V001      : red, red, blue, green
+# V002      : red, red, green, blue
+# 500ms, 50% duty Cycle pulses
+
+# look at auto white balance
+# HSL as a possible method to get around the lightness issue
+
+import cv2
+import numpy as np
+
+def ColourSpace(image):
+    # convert input image to hsv
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # applying filter to remove salt and pepper effect
+    hsv = cv2.medianBlur(hsv, 5)# (3,3), 0)
+    # colour ranges in HSV hard-coded here --> histogram showed 119 average for these numbers. Check in different lighting
+    lower_green = np.array([57,46,155])
+    upper_green = np.array([90,255,255])
+
+    lower_blue = np.array([100,67,221])
+    upper_blue = np.array([120,255,255])
+
+    lower_red = np.array([130,67,0])
+    upper_red = np.array([255,255,255])
+
+    # threshold the hsv image
+    blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    green_mask = cv2.inRange(hsv, lower_green, upper_green)
+    red_mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    # bitwise-AND the mask and frame
+    res_blue = cv2.bitwise_and(image,image,mask = blue_mask)
+    res_green = cv2.bitwise_and(image,image,mask = green_mask)
+    res_red = cv2.bitwise_and(image,image,mask = red_mask)
+
+    # add the red, green and blue together to see each  on the same output feed
+    res = np.add(res_blue,res_green)
+    res = np.add(res,res_red)
+
+    return (res, res_red, res_green, res_blue)
+
+
+
+cap = cv2.VideoCapture(0)
+# initialising counter and startSequence
+i = 0
+startSequence = 0
+error = 0
+# initialising error codes
+Normal = ['R','Z','B','Z','G']
+V001 = ['R', 'Z', 'R', 'Z', 'B', 'Z', 'G']
+V002 = ['R', 'Z', 'R', 'Z', 'G', 'Z', 'B']
+
+Normal = ['R','Z','B','Z','G',      'Z','R','Z','B','Z','G']
+V001 = ['R', 'Z', 'R', 'Z', 'B', 'Z', 'G',      'Z','R', 'Z', 'R', 'Z', 'B', 'Z', 'G']
+V001alt = ['R', 'Z', 'B', 'Z', 'G',      'Z','R', 'Z', 'R', 'Z', 'B', 'Z', 'G']
+V002 = ['R', 'Z', 'R', 'Z', 'G', 'Z', 'B',      'Z','R', 'Z', 'R', 'Z', 'G', 'Z', 'B', ]
+V002alt = ['R', 'Z', 'G', 'Z', 'B',      'Z','R', 'Z', 'R', 'Z', 'G', 'Z', 'B', ]
+
+
+# initialising raw and final arrays
+lightsRaw =[]
+sequence =[]
+xmin, ymin, width, height = 0, 0, 480, 640
+
+while True:
+    k = cv2.waitKey(1) & 0xFF
+    # resetting due to error in sequence detection
+    if error == 99:
+        i = 0
+        startSequence = 0
+        error =0
+        lightsRaw =[]
+        sequence = []
+
+    # read current frame
+    ret, image_np = cap.read()
+    image_np = cv2.medianBlur(image_np, 9)# (3,3), 0)
+
+    # ------------ create mask ----------------#
+    # Create the basic black image
+    mask = np.zeros(image_np.shape, dtype = "uint8")
+    if k == ord('w'):
+        initBB = cv2.selectROI('Output Feed',image_np,fromCenter=False,showCrosshair=True)
+        xmin, ymin, width, height = initBB
+
+    cv2.rectangle(mask, (xmin, ymin), (xmin+width,ymin+height ), (255, 255, 255), -1)
+    image_np = cv2.bitwise_and(image_np,mask)
+
+    # running ColourSpace program, returns combined colours in an array for each frame of video
+    (outputFeed, redArray, greenArray, blueArray) = ColourSpace(image_np)
+    redArray = cv2.medianBlur(redArray, 7)
+    greenArray = cv2.medianBlur(greenArray, 3)
+    blueArray = cv2.medianBlur(blueArray, 5)
+
+    # find the max value of each array, find if it is red, green or blue as other arrays will be 0
+    maxRed = np.amax(redArray)
+    maxGreen = np.amax(greenArray)
+    maxBlue = np.amax(blueArray)
+
+    # make sure the sequence starts at Red for ease
+    # if red, sets StartSequence to 1 for the rest of this loop to run
+    if maxRed >= 170:
+        startSequence = 1
+        if i == 0:
+            print('starting visual error code detection')
+
+    # loop to store colours once red value has been found
+    if startSequence == 1:
+        # check if red, green or blue (or black),
+        # append this to the end of the raw array
+        if maxRed >= 170:
+            #print('Red')
+            lightsRaw.append('R')
+
+        elif maxGreen >= 170:
+            #print('Green')
+            lightsRaw.append('G')
+
+        elif maxBlue >= 170:
+            #print('Blue')
+            lightsRaw.append('B')
+
+        else:
+            #print('Black')
+            lightsRaw.append('Z')
+
+        # check if the current colour is different from the previous
+        if i != 0:
+            if lightsRaw[i] != lightsRaw[i-1]:
+                # if the colour is different add it to the sequence
+                sequence.append(lightsRaw[i])
+                print('sequence:',sequence)
+
+                # first check if the sequence is less than the maximum length of the defined error codes
+                if len(sequence) > max(len(Normal),len(V001),len(V002)):
+                    error = 99
+                    print('incorrect Sequence detected, starting again')
+
+                # and then check if this sequence is equal to any of the known error code sequences
+                elif sequence == Normal:
+                    # RBG
+                    print('sequence:',sequence)
+                    print('error code: Normal Operation')
+                    break
+
+                elif sequence == V001:
+                    # RRBG
+                    print('sequence:',sequence)
+                    print('error code: V001')
+                    break
+
+                elif sequence == V001alt:
+                    # RRBG
+                    print('sequence:',sequence)
+                    print('error code: V001')
+                    break
+
+                elif sequence == V002:
+                    # RRGB
+                    print('sequence:',sequence)
+                    print('error code: V002')
+                    break
+
+                elif sequence == V002alt:
+                    # RRGB
+                    print('sequence:',sequence)
+                    print('error code: V002')
+                    break
+
+        # Things to do just for the first loop iteration
+        elif i == 0:
+            # append the first colour
+            sequence.append(lightsRaw[i])
+
+        i = i + 1
+        # output the resultant processed image
+
+    cv2.imshow('original image', image_np)
+    cv2.imshow('green', greenArray)
+    cv2.imshow('blue', blueArray)
+    cv2.imshow('red', redArray)
+
+    if k == ord('q'):
+        break
+    if k == ord('s'):
+        while(1):
+            if (cv2.waitKey(1) & 0xFF) == ord('s'):
+                break
+cv2.destroyAllWindows()
---
+++ 3rd Year Group Robotics Project/z_objectDetection_Tracking.py
@@ -0,0 +1,202 @@
+import numpy as np
+import os
+import tensorflow as tf
+import imutils
+import time
+import cv2
+import argparse
+
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+from imutils.video import VideoStream
+from imutils.video import FPS
+
+# ------------------------------ # Definitions # ------------------------------ #
+def object_detection(image_np_expanded,detection_limit):
+  
+  image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+  # Each box represents a part of the image where a particular object was detected.
+  boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+  
+
+  # Each score represent how level of confidence for each of the objects.
+  # Score is shown on the result image, together with the class label.
+  scores = detection_graph.get_tensor_by_name('detection_scores:0')
+  classes = detection_graph.get_tensor_by_name('detection_classes:0')
+  num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+  # Actual detection 
+
+  # drawing all boxes defined by model here
+  (boxes, scores, classes, num_detections) = sess.run(
+      [boxes, scores, classes, num_detections],
+      feed_dict={image_tensor: image_np_expanded})
+
+  # # Visualization of the results # #
+  vis_util.visualize_boxes_and_labels_on_image_array(
+      image_np,
+      np.squeeze(boxes),
+      np.squeeze(classes).astype(np.int32),
+      np.squeeze(scores),
+      category_index,
+      use_normalized_coordinates=True,
+      line_thickness=8)
+  
+  box_temp = ((boxes[0])[0])
+  xmin = box_temp[1] * 640
+  xmax = box_temp[3] * 640
+  ymin = box_temp[0] * 480
+  ymax = box_temp[2] * 480
+  width = xmax-xmin
+  height = ymax - ymin
+  
+  return (xmin,ymin,width,height)
+
+# ----------------- # ------------------------------------ # ----------------- #
+
+
+
+
+# -------- # Argument preparation, Multi-Tracker Creation # ------------------ #
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--tracker", type=str, default="csrt",
+	help="OpenCV object tracker type")
+args = vars(ap.parse_args())
+
+# initialize a dictionary that maps strings to their corresponding
+# OpenCV object tracker implementations
+OPENCV_OBJECT_TRACKERS = {
+	"csrt": cv2.TrackerCSRT_create,
+	"kcf": cv2.TrackerKCF_create,
+	"boosting": cv2.TrackerBoosting_create,
+	"mil": cv2.TrackerMIL_create,
+	"tld": cv2.TrackerTLD_create,
+	"medianflow": cv2.TrackerMedianFlow_create,
+	"mosse": cv2.TrackerMOSSE_create
+}
+
+tracker = OPENCV_OBJECT_TRACKERS[args['tracker']]()
+
+
+
+
+
+# -------------------------- # Model preparation # --------------------------- #
+# What model to download.
+MODEL_NAME = 'current_PuckModel'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('training', 'puck_label.pbtxt') #changed to training, where the label map is
+
+NUM_CLASSES = 1
+
+# ## Load a (frozen) Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+# ## Loading label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+
+
+
+
+
+# ------------------------------ # Main # ------------------------------------ #
+
+
+print("[INFO] starting video stream...")
+cap = cv2.VideoCapture(0)
+tracker = cv2.TrackerCSRT_create()
+detection_limit = 5
+
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    while True:
+      # read current frame from camera
+      ret, image_np = cap.read()
+
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+
+      # object detection happens here,
+      # box defined is the 1st element (highest scoring detection) 
+      # This will have to change to be the bounding box for the broken PUCK
+      xmin, ymin, width, height = object_detection(image_np_expanded,detection_limit)
+      
+      
+#      cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+      # display output feed
+      cv2.putText(image_np,'Detection Algorithm',(30,50),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255),2)
+      cv2.putText(image_np,'click q to enter Tracking Stage with detected ROI',(30,70),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255),2)
+      cv2.putText(image_np,'or click s to manually select ROI',(30,90),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255),2)
+      cv2.imshow('Output Feed', image_np)
+      # optional delay for debugging purposes
+      time.sleep(0)
+      
+      # button press to exit detection stage and enter tracking stage
+      key = cv2.waitKey(1) & 0xFF
+      if key == ord('q'):
+        manualROI = 0 
+        break
+        #cv2.destroyAllWindows()
+      elif key == ord('s'):
+        manualROI = 1
+        break
+      
+    
+       
+    # now begin to track the defined bounding box
+    first_run = 1
+    initBB = None
+    while True:
+      # read current frame from camera
+      ret, image_np = cap.read()
+      
+      # check and run if there is an object to track (bounding box is initialised)
+      if initBB is not None:
+        # grab the updated bounding box for the current frame (tracking)
+        (success, box) = tracker.update(image_np)
+        
+        # if tracking was successful, draw this bounding box on output feed
+        if success:
+          (x,y,w,h) = [int(v) for v in box]
+          cv2.rectangle(image_np,(x,y),(x+w,y+h),(0,255,0),2)
+      
+      # displaying output feed   
+      cv2.putText(image_np,'Tracking Algorithm, click q to exit',(30,50),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255),2)
+      cv2.imshow('Output Feed',image_np)
+      
+      # this is only run for the first detected bounding box
+      if first_run == 1:
+        # option to manually select ROI if detection isnt working (backup)
+        if manualROI == 1:
+          initBB = cv2.selectROI('Output Feed',image_np,fromCenter=False,showCrosshair=True)
+        else:
+        # initialising bounding box with detected box
+          initBB = (xmin,ymin,width,height)
+          
+        tracker.init(image_np,initBB)
+        first_run = 0
+#        print(initBB)
+      
+      # exit this loop under 'q' button press
+      key= cv2.waitKey(1) & 0xFF
+      if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
